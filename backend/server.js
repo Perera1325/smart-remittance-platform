254c72fbc0493c5dd3d4d5db675cf2bbee3e3c39
Vinod Ravindu Perera <bamunuge2002@gmail.com>
2026-03-27 19:46:14 +0530

    Integrate currency routes into main server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
-
 const app = express();
+
+const currencyRoutes = require("./routes/currencyRoutes");
+
 const PORT = 3000;
 
 app.use(express.json());
 
-// Health check route
+// routes
+app.use("/api", currencyRoutes);
+
 app.get("/health", (req, res) => {
     res.json({ status: "Server is running" });
 });
